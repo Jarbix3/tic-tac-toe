1e75b00e59f21b66aae16cae589fd561ca599a2b
Jarbix3 <52262552+Jarbix3@users.noreply.github.com>
2024-07-24 21:03:07 -0300

    now winner works correctly

--- lib/board.rb
+++ lib/board.rb
@@ -4,9 +4,7 @@ class Board # rubocop:disable Style/Documentation
   attr_accessor :board_arr, :symbol, :game_over
 
   def initialize(symbol)
-    @board_arr = [0, 1, 2, 3,
-                  4, 5, 6,
-                  7, 8, 9]
+    @board_arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     @game_over = false
     @symbol = symbol
   end
--- main.rb
+++ main.rb
@@ -18,33 +18,50 @@ def start # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
     break if win == true
 
     choice = ''
+    symbol = ''
+    name = ''
 
     if i.odd?
       choice = player_one.make_move
       board.draw_choice(choice.to_i, player_one.symbol)
       board.display
-      win = winner(board.board_arr, player_one.symbol, player_one.name, i)
+      name = player_one.name
+      symbol = player_one.symbol
 
     end
     if i.even?
       choice = player_two.make_move
       board.draw_choice(choice.to_i, player_two.symbol)
       board.display
-      win = winner(board.board_arr, player_two.symbol, player_one.name, i)
+      name = player_two.name
+      symbol = player_two.symbol
     end
-
+    win = winner(board.board_arr, symbol, name, i)
     i += 1
 
   end
 end
 
 def winner(board_arr, symbol, plyr_name, turns) # rubocop:disable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity,Metrics/AbcSize,Metrics/MethodLength
-  row1 = board_arr[1..3], row2 = board_arr[4..6]
-  row3 = board_arr[7..9], col1 = [board_arr[1], board_arr[4], board_arr[7]]
-  col2 = [board_arr[2], board_arr[5], board_arr[8]], col3 = [board_arr[3], board_arr[6], board_arr[9]],
-  diag1 = [board_arr[1], board_arr[5], board_arr[9]], diag2 = [board_arr[3], board_arr[5], board_arr[7]] # rubocop:disable Layout/ArrayAlignment
+  row1 = [board_arr[1], board_arr[2], board_arr[3]]
+  row2 = [board_arr[4], board_arr[5], board_arr[6]]
+  row3 = [board_arr[7], board_arr[8], board_arr[9]]
+  col1 = [board_arr[1], board_arr[4], board_arr[7]]
+  col2 = [board_arr[2], board_arr[5], board_arr[8]]
+  col3 = [board_arr[3], board_arr[6], board_arr[9]]
+  diag1 = [board_arr[1], board_arr[5], board_arr[9]]
+  diag2 = [board_arr[3], board_arr[5], board_arr[7]]
 
-  if row1.all? { |n| n == symbol } || row2.all? { |n| n == symbol } || row3.all? { |n| n == symbol }
+  if row1.all? { |n| n == symbol }
+    puts "\t\t\t\t#{plyr_name} wins!!"
+    return true
+  end
+  if row2.all? { |n| n == symbol }
+    puts "\t\t\t\t#{plyr_name} wins!!"
+    return true
+  end
+
+  if row3.all? { |n| n == symbol }
     puts "\t\t\t\t#{plyr_name} wins!!"
     return true
   end
