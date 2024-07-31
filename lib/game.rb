1b2493d6c6bde66c82f0234d4d9ca2403493299f
Jarbix3 <52262552+Jarbix3@users.noreply.github.com>
2024-07-31 20:21:36 -0300

    QOL code changes

---
+++ lib/game.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require_relative 'player'
+require_relative 'board'
+# Plays the game and decides the winner
+class Game
+  def start
+    player_one = Player.new(1, 'X')
+    player_two = Player.new(2, 'O')
+
+    board = Board.new(player_one.symbol)
+
+    play_turns(board, player_one, player_two)
+  end
+
+  def winner(board_arr, symbol, plyr_name, turns) # rubocop:disable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity,Metrics/AbcSize,Metrics/MethodLength
+    row1 = [board_arr[1], board_arr[2], board_arr[3]]
+    row2 = [board_arr[4], board_arr[5], board_arr[6]]
+    row3 = [board_arr[7], board_arr[8], board_arr[9]]
+    col1 = [board_arr[1], board_arr[4], board_arr[7]]
+    col2 = [board_arr[2], board_arr[5], board_arr[8]]
+    col3 = [board_arr[3], board_arr[6], board_arr[9]]
+    diag1 = [board_arr[1], board_arr[5], board_arr[9]]
+    diag2 = [board_arr[3], board_arr[5], board_arr[7]]
+
+    if row1.all? { |n| n == symbol }
+      puts "\t\t\t\t#{plyr_name} wins!!"
+      return true
+    end
+    if row2.all? { |n| n == symbol }
+      puts "\t\t\t\t#{plyr_name} wins!!"
+      return true
+    end
+
+    if row3.all? { |n| n == symbol }
+      puts "\t\t\t\t#{plyr_name} wins!!"
+      return true
+    end
+
+    if col1.all? { |n| n == symbol } || col2.all? { |n| n == symbol } || col3.all? { |n| n == symbol }
+      puts "\t\t\t\t#{plyr_name} wins!!"
+      return true
+    end
+
+    if diag1.all? { |n| n == symbol } || diag2.all? { |n| n == symbol }
+      puts "\t\t\t\t#{plyr_name} wins!!"
+      return true
+    end
+
+    return unless turns == 9
+
+    puts "\t\t\t\tIt's a tie!!!"
+    false
+  end
+
+  def play_turns(board, player_one, player_two)
+    win = false
+    board.display
+    i = 1
+    while i <= 9
+      break if win == true
+
+      if i.odd?
+        choice = player_one.make_move
+        board.draw_choice(choice.to_i, player_one.symbol)
+        board.display
+        name = player_one.name
+        symbol = player_one.symbol
+
+      end
+      if i.even?
+        choice = player_two.make_move
+        board.draw_choice(choice.to_i, player_two.symbol)
+        board.display
+        name = player_two.name
+        symbol = player_two.symbol
+      end
+      win = winner(board.board_arr, symbol, name, i)
+      i += 1
+
+    end
+  end
+end
